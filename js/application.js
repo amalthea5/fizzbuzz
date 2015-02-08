a9ae54e24afada707cde62746ca5777fdf6331c2
amalthea5 <erin.knapp@gmail.com>
2015-02-08 00:27:16 -0500

    Changed content from class to id

--- js/application.js
+++ js/application.js
@@ -12,16 +12,16 @@ $(document).ready(function() {
 
 	for (var i=1; i <=value; i++) {
 		if((i%3==0) && (i%5==0)) {
-			$(".content").append("<button class='fizzbuzz'>fizzbuzz</button>");
+			$("#content").append("<button class='fizzbuzz'>fizzbuzz</button>");
 		}
 		else if (i%3==0) {
-			$(".content").append("<button class='fizz'>fizz</button>");
+			$("#content").append("<button class='fizz'>fizz</button>");
 		}
 		else if (i%5==0) {
-			$(".content").append("<button class='buzz'>buzz</button>");
+			$("#content").append("<button class='buzz'>buzz</button>");
 		}
 		else {
-			$(".content").append("<button class='plain'>" + i + " </button>");
+			$("#content").append("<button class='plain'>" + i + " </button>");
 		}
 	}
 
